b2675a7eefca3e0839044af3b4012728dd4e6358
Flaviamol <flaviacm.de@gmail.com>
2021-10-21 11:22:45 +0200

    html basic of app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <button className="btn btn-primary">Hello</button>
+        <Weather />
         <footer>
           This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a> and is open-sourced on{" "}
           <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a>
