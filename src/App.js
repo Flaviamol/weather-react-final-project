2afb88fcbc8aa7f7574dd33d5744436816d57eb3
Flaviamol <flaviacm.de@gmail.com>
2021-10-21 10:18:43 +0200

    add github an linkedin URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a> and is open-sourced on{" "}
+        <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a>
+      </footer>
+    </div>
   );
 }
 
