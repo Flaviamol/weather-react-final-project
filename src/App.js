766ad763563ba37de40baa62dd095f6f325d7160
Flaviamol <flaviacm.de@gmail.com>
2021-10-21 10:54:10 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a> and is open-sourced on{" "}
-        <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a> and is open-sourced on{" "}
+          <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
