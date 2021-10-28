bb4c0276590651070b0ea18197f9ed77169bf1b3
Flaviamol <flaviacm.de@gmail.com>
2021-10-28 15:33:53 +0200

    WeatherForecastDay
    function WeatherForecastDay need to return something

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <div className="container">
         <Weather defaultCity="London"/>
         <footer>
-          This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a> and is open-sourced on{" "}
-          <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a>
+          This project was coded by <a href="https://www.linkedin.com/in/flávia-caiafa-mól-00595bb1/" target="_blank" rel="noreferrer">Flávia Mól</a>, is open-sourced on{" "}
+          <a href="https://github.com/Flaviamol/weather-react-final-project" target="_blank" rel="noreferrer" >GitHub</a> and hosted on <a href="https://thirsty-fermat-670a95.netlify.app" target="_blank" rel="noreferrer">Netlify</a>
         </footer>
       </div>
     </div>
