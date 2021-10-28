03539ff5c2ecea23b1b643e93eb70b83b272d2e8
Flaviamol <flaviacm.de@gmail.com>
2021-10-28 15:23:42 +0200

    Update WeatherForecast.js
    make functional the forecast when the location change, the data change also

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
-import { useState } from "react/cjs/react.development";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props){
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+      }, [props.coordinates]);
 
     function handleResponse(response){
         setForecast(response.data.daily);
